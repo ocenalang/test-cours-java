d831c2d4d49986d89726e9b95200a02d44382bd5
Oce_na <langletoce@gmail.com>
2026-03-11 11:53:34 +0100

    Refactor Etudiant class and simplify constructors

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -2,140 +2,53 @@ import java.util.ArrayList;
 
 public class Etudiant {
 
-    // attributs : PRIVÉS
     private String nom;
     private int anneeNaissance;
     private ArrayList<Float> notes;
-    private int id;
-    private double taille;
 
-    // méthodes
-
-    // constructeurs
-    // constructeur par défaut
-    public Etudiant() {
-        System.out.println("Constructeur par défaut");
-        this.nom = "John";
-        this.anneeNaissance = 2007;
-        this.notes = new ArrayList();
-        this.id = 1;
-        this.taille = 1.5;
+    // constructeur
+    public Etudiant(String nom, int anneeNaissance) {
+        this.nom = nom;
+        this.anneeNaissance = anneeNaissance;
+        this.notes = new ArrayList<>();
     }
 
-    // constructeur paramétré
-    public Etudiant(String n, int a) {
-        System.out.println("Constructeur paramétré 1");
-        this.nom = n;
-        this.anneeNaissance = a;
-        this.notes = new ArrayList();
-        this.id = 1;
-        this.taille = 1.5;
+    // ajouter une note
+    public void ajoutNote(float note) {
+        notes.add(note);
     }
 
-    public Etudiant(int a) {
-        System.out.println("Constructeur paramétré 2");
-        this.nom = "Bob";
-        this.anneeNaissance = a;
-        this.notes = new ArrayList<Float>();
-        this.id = 1;
-        this.taille = 1.5;
-    }
-
-    public Etudiant(String n, int a, int i, double t) {
-        this.nom = n;
-        this.anneeNaissance = a;
-        this.id = i;
-        this.taille = t;
-        this.notes = new ArrayList<Float>();
-    }
-
-    // accesseurs = GETTER + SETTER (pour chaque attribut)
-    public String getNom() {
-        return this.nom;
-    }
-
-    public void setNom(String n) {
-        if(n != "f*ck")
-            this.nom = n;
-    }
-
-    public int getAnneeNaissance() {
-        return this.anneeNaissance;
-    }
-
-    public void setAnneeNaissance(int a) {
-        this.anneeNaissance = a;
-    }
-
-    public ArrayList<Float> getNotes() {
-        return this.notes;
-    }
-
-    public void setNotes(ArrayList<Float> n) {
-        this.notes = n;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int i) {
-        this.id = i;
-    }
-
-    public double getTaille() {
-        return this.taille;
-    }
-
-    public void setTaille(double t) {
-        this.taille = t;
-    }
-
-    @Override
-    public String toString() {
-        return "Etudiant s'appelant "+ this.nom +
-                " et qui est né en " + this.anneeNaissance +
-                " ayant l'identifiant " + this.id +
-                " et qui mesure " + this.taille + " mètres";
-    }
-
-    public int age() {
-        return 2026 - this.anneeNaissance;
-    }
-    // met une note
-    public void ajoutNote(float n) {
-        this.notes.add(n);
-    }
-
-    // affiche tt note
+    // afficher les notes
     public void voirNotes() {
-        for(int i = 0; i < this.notes.size(); i++) {
-            System.out.println("Note " + i + " : " + this.notes.get(i));
+        for(int i = 0; i < notes.size(); i++) {
+            System.out.println("Note " + i + " : " + notes.get(i));
         }
     }
 
-    // affiche tel not
-    public float avoirNote(int i) {
-        return this.notes.get(i);
+    // modifier une note
+    public void modifierNote(int i, float note) {
+        if(i >= 0 && i < notes.size()) {
+            notes.set(i, note);
+        }
     }
 
-    // modifi
-    public void modifierNote(int i, float n) {
-        this.notes.set(i, n);
-    }
-
-    // moyenne calcul
+    // calculer la moyenne
     public float moyenne() {
+
         float somme = 0;
 
-        for(int i = 0; i < this.notes.size(); i++) {
-            somme += this.notes.get(i);
+        for(int i = 0; i < notes.size(); i++) {
+            somme += notes.get(i);
         }
 
-        if(this.notes.size() == 0) {
+        if(notes.size() == 0) {
             return 0;
         }
 
-        return somme / this.notes.size();
+        return somme / notes.size();
+    }
+
+    public String getNom() {
+        return nom;
     }
 }
