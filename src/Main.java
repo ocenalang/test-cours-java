8625dd34087a102a440058eb1c830cf59cc1c038
Oce_na <langletoce@gmail.com>
2026-03-11 10:44:52 +0100

    Refactor Etudiant class and main method usage

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,47 @@
-import java.util.ArrayList;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+        public class Main {
 
-public class Etudiant {
+            public static void main(String[] args) {
 
-    // attributs : PRIVÉS
-    private String nom;
-    private int anneeNaissance;
-    private ArrayList<Float> notes;
-    private int id;
-    private double taille;
+                // variable pour stocker un Etudiant
+                // Déclaration
+        Etudiant etu;
+        // Initialisation / construction
+        etu = new Etudiant();
 
-    // méthodes
+        Etudiant etu2 = new Etudiant("Bastien", 1990);
+        Etudiant etu3 = new Etudiant(2002);
 
-    // constructeurs
-    // constructeur par défaut
-    public Etudiant() {
-        System.out.println("Constructeur par défaut");
-        this.nom = "John";
-        this.anneeNaissance = 2007;
-        this.notes = new ArrayList();
-        this.id = 1;
-        this.taille = 1.5;
+        System.out.println("Le premier etudiant s'appelle " + etu.getNom() );
+        etu.setNom("Claude");
+        System.out.println("Le premier etudiant s'appelle " + etu.getNom() );
+
+
+        // etu3.nom = "f*ck";
+        System.out.println(etu3.getNom());
+        etu3.setNom("f*ck");
+        System.out.println(etu3.getNom());
+
+        System.out.println(etu.toString());
+
+        System.out.println(etu.age());
+        System.out.println(etu2.age());
+        System.out.println(etu3.age());
+        //met les note
+        etu.ajoutNote(12);
+        etu.ajoutNote(15);
+        etu.ajoutNote(9);
+
+        etu.voirNotes();
+
+        System.out.println("La note 1 est : " + etu.avoirNote(1));
+
+        etu.modifierNote(1, 18);
+
+        etu.voirNotes();
+
+        System.out.println("Moyenne : " + etu.moyenne());
     }
 
-    // constructeur paramétré
-    public Etudiant(String n, int a) {
-        System.out.println("Constructeur paramétré 1");
-        this.nom = n;
-        this.anneeNaissance = a;
-        this.notes = new ArrayList();
-        this.id = 1;
-        this.taille = 1.5;
-    }
-
-    public Etudiant(int a) {
-        System.out.println("Constructeur paramétré 2");
-        this.nom = "Bob";
-        this.anneeNaissance = a;
-        this.notes = new ArrayList<Float>();
-        this.id = 1;
-        this.taille = 1.5;
-    }
-
-    public Etudiant(String n, int a, int i, double t) {
-        this.nom = n;
-        this.anneeNaissance = a;
-        this.id = i;
-        this.taille = t;
-        this.notes = new ArrayList<Float>();
-    }
-
-    // accesseurs = GETTER + SETTER (pour chaque attribut)
-    public String getNom() {
-        return this.nom;
-    }
-
-    public void setNom(String n) {
-        if(n != "f*ck")
-            this.nom = n;
-    }
-
-    public int getAnneeNaissance() {
-        return this.anneeNaissance;
-    }
-
-    public void setAnneeNaissance(int a) {
-        this.anneeNaissance = a;
-    }
-
-    public ArrayList<Float> getNotes() {
-        return this.notes;
-    }
-
-    public void setNotes(ArrayList<Float> n) {
-        this.notes = n;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int i) {
-        this.id = i;
-    }
-
-    public double getTaille() {
-        return this.taille;
-    }
-
-    public void setTaille(double t) {
-        this.taille = t;
-    }
-
-    @Override
-    public String toString() {
-        return "Etudiant s'appelant "+ this.nom +
-                " et qui est né en " + this.anneeNaissance +
-                " ayant l'identifiant " + this.id +
-                " et qui mesure " + this.taille + " mètres";
-    }
-
-    public int age() {
-        return 2026 - this.anneeNaissance;
-    }
 }
